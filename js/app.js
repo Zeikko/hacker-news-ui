d575f0874ee33311334fc762015b93fe80b7fb54
Hannu Lyytikainen <hannu.lyytikainen@gofore.com>
2015-09-09 10:50:02 +0300

    initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Fetch 10 latest of hacker news top stories
+ * and print them on page with title and link.
+ */
+
+var hn = (function() {
+
+  const hnTopStories = "https://hacker-news.firebaseio.com/v0/topstories.json";
+  // hacker news item url form: https://hacker-news.firebaseio.com/v0/item/<item-id>.json
+  const hnItem = "https://hacker-news.firebaseio.com/v0/item/"
+
+  return {
+    printNews: function () {
+
+    }
+  }
+}());
