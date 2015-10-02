1a9b3390ffd8ec9dfb66f5527b5deff7375f356c
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2015-10-02 07:59:04 +0300

    Fetch 10 top stories

--- js/app.js
+++ js/app.js
@@ -9,9 +9,42 @@ var hn = (function() {
   // hacker news item url form: https://hacker-news.firebaseio.com/v0/item/<item-id>.json
   const HN_ITEM = "https://hacker-news.firebaseio.com/v0/item/"
 
-  return {
-    printNews: function () {
+  function fetchStory(callback, id) {
+    $.ajax({
+      url: HN_ITEM + id + '.json',
+      dataType: 'json',
+      success: function(data) {
+        callback(null, data);
+      }
+    })
+  }
 
+  function fetchLatestStories(callback, limit) {
+    var stories = [];
+    $.ajax({
+      url: HN_TOP_STORIES,
+      dataType: 'json',
+      success: function(data) {
+        storyIds = _.slice(data, 0, limit);
+        async.each(storyIds, function(storyId, callback) {
+          fetchStory(function(err, story) {
+            stories.push(story);
+            callback();
+          }, storyId);
+        }, function(err, data) {
+          callback(null, stories)
+        });
+      }
+    })
+  }
+
+  return {
+    printNews: function() {
+      fetchLatestStories(function(err, stories) {
+        _.forEach(stories, function(story) {
+          $('body').append('<a href="' + story.url + '">' + story.title +'</a><br />')
+        })
+      }, 10)
     }
   }
 }());
